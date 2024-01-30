9e0bdb9485a4d4eae3fbf5e65167bbc261fd8ffe
BakrAldubai <eng.bakraldubai@gmail.com>
2024-01-30 14:44:30 +0300

    feat(Academicx): adding app settings

---
+++ academix/academix/doctype/academix_settings/academix_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, BakrAldubai and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Academix Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
